73e306f27cfa2d592055967d7a2da47d40a06c2d
cristin jara <cristin.jara36@gmail.com>
2020-05-16 20:49:02 -0400

    .

--- SigebiWeb/src/main/java/com/sigebi/entity/Personas.java
+++ SigebiWeb/src/main/java/com/sigebi/entity/Personas.java
@@ -12,7 +12,6 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
