402ca204e30069d4a91955b5f4f0883b0c2bd83e
letimendieta <letiziag294@gmail.com>
2020-06-29 09:00:29 -0400

    Login backend

---
+++ SigebiWeb/src/main/java/com/sigebi/security/service/UsuarioService.java
@@ -0,0 +1,30 @@
+package com.sigebi.security.service;
+
+import com.sigebi.security.entity.Usuario;
+import com.sigebi.security.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+        return usuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+
+    public boolean existsByNombreUsuario(String nombreUsuario){
+        return usuarioRepository.existsByNombreUsuario(nombreUsuario);
+    }
+
+
+    public void save(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+}
--- SigebiWeb/src/main/java/com/sigebi/service/UsuariosService.java
+++ SigebiWeb/src/main/java/com/sigebi/service/UsuariosService.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +26,7 @@ public class UsuariosService implements UserDetailsService{
 	private IUsuariosDao repo;
 	
 	@Autowired
-	private BCryptPasswordEncoder encoder;
+	private PasswordEncoder encoder;
 		
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
