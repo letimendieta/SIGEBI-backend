3c9dc19e79cda5291f4911e72a5f306cb08e2637
crisRafJar <crstin.jara36@gmail.com>
2020-07-30 18:44:04 -0400

    Ajuste en UsuariosService.

--- SigebiWeb/src/main/java/com/sigebi/service/UsuariosService.java
+++ SigebiWeb/src/main/java/com/sigebi/service/UsuariosService.java
@@ -1,57 +1,33 @@
 package com.sigebi.service;
 
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import org.aspectj.bridge.AbortException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.sigebi.dao.IUsuariosDao;
 import com.sigebi.entity.Usuarios;
 
-@Service
-public class UsuariosService implements UserDetailsService{
 
-	@Autowired
-	private IUsuariosDao repo;
+public interface UsuariosService {
+
+	public List<Usuarios> findAll();
 	
-	@Autowired
-	private BCryptPasswordEncoder encoder;
-		
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		Usuarios us = repo.findByCodigoUsuario(username);
-		
-		List<GrantedAuthority> roles = new ArrayList<>();
-		roles.add(new SimpleGrantedAuthority("ADMIN"));
-		
-	    UserDetails userDet = new User(us.getCodigoUsuario(), us.getPassword(), roles);
-	    
-	    return userDet;
-	}
-
-	public List<GrantedAuthority> buildGrante(byte rol){
-		return null;
-	}
-	@Transactional
-	public void crearUsuario(Usuarios usuario){
-		try {
-			String encodePass = encoder.encode(usuario.getPassword());	
-			usuario.setPassword(encodePass);
-			repo.save(usuario);
-		} catch (Exception e) {
-			throw new AbortException("Ocurrio un error al guardar el usuario: " + e.getMessage());
-		}
-		
-	}
+	public Usuarios findById(int id);	
+	
+	public Usuarios guardar(Usuarios usuario) throws Exception;
+	
+	public Usuarios actualizar(Usuarios usuario) throws Exception;
+	
+	public void delete(int id);
+	
+	public List<Usuarios> buscar(Date fromDate, Date toDate, Usuarios usuario, List<Integer> personasId, Pageable pageable);
+	
+	public UserDetails loadUserByUsername(String username);
+	
+	public List<GrantedAuthority> buildGrante(byte rol);
+	
+	public void crearUsuario(Usuarios usuario);
+	
 }
